7cdcda6547e47ceb7a669ffe82af0dd97550898f
kevin4191 <kevingennissen70@gmail.com>
2024-12-20 14:30:24 +0100

    20 dec

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -8,32 +8,18 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
   ],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
-  public proficienciesFrontend: string[] = [
-    'CSS',
-    'HTML',
-    'Javascript',
-    'Typescript',
-    'Angular',
-    'Tailwind CSS',
-    'Bootstrap',
-    'SASS',
-    'Material Design',
-  ]
-  public proficienciesBackend: string[] = [
-    'Java',
-    'Spring Boot',
-  ]
-  public proficienciesTools: string[] = [
-    'Git',
-    'Github',
-    'Wordpress',
-    'DevOps',
-    'VS Code',
-    'Yarn',
-    'NPM',
+  public projects: { name: string, tags: string[], image: string}[] = [
+    { name: 'Project 1', tags: ['tag1', 'tag2'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 2', tags: ['tag1', 'tag3'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 3', tags: ['tag2', 'tag4'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 4', tags: ['tag3', 'tag4'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 5', tags: ['tag1', 'tag2'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 6', tags: ['tag1', 'tag3'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 7', tags: ['tag2', 'tag4'], image: '/placeholder_carousel_card.jpg' },
+    { name: 'Project 8', tags: ['tag3', 'tag4'], image: '/placeholder_carousel_card.jpg' },
   ]
 
 }
